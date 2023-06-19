a7c223f38f3cdd4de8998f3c1186a9a17a3f6bf8
nigrojin <nigrojin@naver.com>
2023-06-19 10:24:22 +0900

    6/19

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -48,16 +48,57 @@ export default function App() {
   }
 
   // 할일 삭제
-  function deleteTask(id) {}
+  function deleteTask(id) {
+    // tasks에서 전달받은 id와 일치하는 id를 가진 task를 제거한다
+    const remainingTasks = tasks.filter(task => task.id !== id );
+
+    // tasks 업데이트
+    setTasks(remainingTasks);
+
+    // 로컬스토리지 동기화
+    saveDoc(remainingTasks);
+  }
 
   // 할일의 상태 변경
-  function toggleTaskCompleted(id) {}
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // 전달받은 id와 일치하는 id를 가진 task의 completed를
+      // 현재상태의 반대값을 적용한다
+      if (task.id === id) {
+        return { ...task, completed: !task.completed }
+      }
+      // 나머지는 그대로 리턴한다
+      return task;
+    })
+
+    // tasks 업데이트
+    setTasks(updatedTasks);
+    // 로컬 스토리지 동기화
+    saveDoc(updatedTasks)
+  }
 
   // 할일 업데이트
-  function editTask(id, newName) {}
+  function editTask(id, newName) {
+    const editedTasks = tasks.map(task => {
+      // 전달받은 id와 일치하는 id를 가진 task의 name을
+      // newName으로 업데이트한다
+      if (task.id === id) {
+        return { ...task, name: newName };
+      }
+      // 나머지는 그대로 리턴한다
+      return task;
+    })
+
+    // tasks 업데이트
+    setTasks(editedTasks);
+
+    // 로컬스토리지 동기화
+    saveDoc(editedTasks)
+  }
 
   // 필터버튼
   const filterButtons = FILTER_NAMES.map(name => (
+    // FilterButton 컴포넌트 재사용
     <FilterButton
       key={name}
       name={name}
@@ -68,6 +109,7 @@ export default function App() {
 
   // 할일 목록
   const taskList = tasks.filter(FILTER_MAP[filter]).map(task => (
+    // Todo 컴포넌트 재사용
     <Todo
       key={task.id}
       id={task.id}
--- todo-app/src/components/FilterButton.js
+++ todo-app/src/components/FilterButton.js
@@ -1,1 +1,14 @@
-export default function FilterButton() {}
+import { useState } from "react";
+
+export default function FilterButton({ name, isPressed, setFilter }) {
+  return (
+    <button
+      className={"border-2 border-black p-1 w-1/3 font-semibold " + (
+        isPressed && "bg-black text-white"
+      )}
+      onClick={() => setFilter(name)}
+    >
+      {name}
+    </button>
+  )
+}
