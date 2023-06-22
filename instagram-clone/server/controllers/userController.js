72bcef9553b373d2f20bc68eaeaddeed0129935f
nigrojin <nigrojin@naver.com>
2023-06-22 21:46:09 +0900

    6/22

--- instagram-clone/server/controllers/userController.js
+++ instagram-clone/server/controllers/userController.js
@@ -2,13 +2,153 @@ const User = require('../models/User');
 const fileHandler = require('../utils/fileHandler');
 const { body, validationResult } = require('express-validator');
 
+// 입력 데이터의 유효성 검사 미들웨어
+
+// 1 유저이름 유효성 검사
+const isValidUsername = () => body('username')
+  .trim()
+  .isLength({ min: 5 }).withMessage('Username must be at least 5 characters')
+  .isAlphanumeric().withMessage("Username is only allowed in alphabet and number.")
+
+// 2 이메일 유효성 검사
+const isValidEmail = () => body('email')
+  .trim()
+  .isEmail().withMessage('E-mail is not valid')
+
+// 3 비밀번호 유효성 검사
+const isValidPassword = () => body('password')
+  .trim()
+  .isLength({ min: 5 }).withMessage('Password must be at least 5 characters')
+
+// 4 이메일 중복확인
+const emailInUse = async (email) => {
+  // 입력된 이메일로 유저를 검색한다
+  const user = await User.findOne({ email });
+  
+  // 유저가 존재하는 경우
+  if (user) {
+    return Promise.reject('E-mail is already in use');
+  }
+}
+
+// 5 유저이름 중복확인
+const usernameInUse = async (username) => {
+  // 입력된 유저이름으로 유저를 검색한다
+  const user = await User.findOne({ username });
+
+  // 유저가 존재하는 경우
+  if (user) {
+    return Promise.reject('Username is already in use');
+  }
+}
+
+// 6 이메일 존재 확인
+const doesEmailExists = async (email) => {
+  // 입력된 이메일로 유저를 검색한다
+  const user = await User.findOne({ email });
+
+  // 유저가 존재하지 않을 경우
+  if (!user) {
+    return Promise.reject('User is not found');
+  }
+}
+
+// 7 비밀번호 검사
+const doesPasswordMatch = async (password, { req }) => {
+  // 입력된 이메일로 유저를 검색한다
+  const email = req.body.email;
+  const user = await User.findOne({ email });
+  
+  // 검색된 유저의 비밀번호와 입력된 비밀번호를 비교한다
+  if (!user.checkPassword(password)) {
+    return Promise.reject('Password does not match');
+  }
+}
 
 // 유저 생성
-exports.create = [];
+exports.create = [
+  isValidUsername().custom(usernameInUse),
+  isValidEmail().custom(emailInUse),
+  isValidPassword(),
+  async (req, res, next) => {
+    try {
+      // 유효성 검사 결과
+      const errors = validationResult(req);
+
+      // 검사 실패
+      if (!errors.isEmpty()) {
+        const err = new Error();
+        err.errors = errors.array();
+        err.status = 400; // 400 BadRequest
+        throw err;
+      }
+
+      // 클라이언트가 전송한 데이터
+      const { email, fullName, username, password } = req.body;
+
+      // 유저 생성
+      const user = new User();
+
+      user.email = email;
+      user.fullName = fullName;
+      user.username = username;
+      user.setPassword(password); // 비밀번호 암호화
+
+      await user.save();
+
+      // 클라이언트에게 생성한 유저 데이터를 전송한다
+      res.json({ user });
+
+    } catch (error) {
+      // 에러핸들러에게 에러를 전달한다
+      next(error)
+    }
+  }
+];
 
 // 유저 정보 수정
 exports.update = [];
 
 // 로그인
-exports.login = [];
+exports.login = [
+  isValidEmail().custom(doesEmailExists),
+  isValidPassword().custom(doesPasswordMatch),
+  async (req, res, next) => {
+    try {
+      // 유효성 검사 결과
+      const errors = validationResult(req);
+
+      // 검사 실패
+      if (!errors.isEmpty()) {
+        const err = new Error();
+        err.errors = errors.array();
+        err.status = 401; // 401 Unauthorized(인증 실패)
+        throw err;
+      }
+
+      const { email } = req.body;
+
+      // 클라이언트가 전송한 이메일로 유저를 검색한다
+      const _user = await User.findOne({ email });
+
+      // 로그인 토큰 생성
+      const token = _user.generateJWT();
+  
+      const user = {
+        username: _user.username,
+        email: _user.email,
+        fullName: _user.fullName,
+        avatar: _user.avatar,
+        bio: _user.bio,
+        token
+      }
+  
+      // 클라이언트에게 유저데이터와 토큰을 전송한다
+      res.json({ user })
+
+    } catch (error) {
+      next(error)
+    }
+  }
+];
 
--- instagram-clone/server/routes/index.js
+++ instagram-clone/server/routes/index.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router();
+const passport = require("passport");
+const auth = passport.authenticate("jwt", { session: false });
+const jwtStrategy = require("../auth/jwtStrategy");
+const articleController = require("../controllers/articleController");
+const commentController = require("../controllers/commentController");
+const userController = require("../controllers/userController");
+const profileController = require("../controllers/profileController");
+passport.use(jwtStrategy);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+/* INDEX */
+// router.HTTPRequestMethod(URL, Controller)
+router.get('/', (req, res) => {
+  // req(request): 요청 객체
+  // res(response): 응답 객체
+
+  // 서버의 응답
+  res.json({ message: "API Server - INDEX PAGE" });
+})
+
+/* USER */
+router.post('/users', userController.create); 
+router.put('/user', auth, userController.update); 
+router.post('/user/login', userController.login); 
+
+/* ARTICLE */
+router.get('/feed', auth, articleController.feed) 
+router.get('/articles', auth, articleController.find)
+router.post('/articles', auth, articleController.create)  
+router.get('/articles/:id', auth, articleController.findOne)
+router.delete('/articles/:id', auth, articleController.delete) 
+router.post('/articles/:id/favorite', auth, articleController.favorite) 
+router.delete('/articles/:id/favorite', auth, articleController.unfavorite)
+
+/* COMMENT */
+router.get('/articles/:id/comments', auth, commentController.find) 
+router.post('/articles/:id/comments', auth, commentController.create) 
+router.delete('/comments/:id', auth, commentController.delete) 
+
+/* PROFILE */
+router.get('/profiles', auth, profileController.find); 
+router.get('/profiles/:username', auth, profileController.findOne)
+router.post('/profiles/:username/follow', auth, profileController.follow) 
+router.delete('/profiles/:username/follow', auth, profileController.unfollow)
 
 module.exports = router;
